b25ce1ffa06eddc5d6c7f0cac9485775488f1206
AsaTyr <43162495+AsaTyr2018@users.noreply.github.com>
2024-02-28 08:44:11 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'LSN KASM Repository',
+    description: 'Lets store some Container.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://asatyr2018.github.io/kasm_lsn_repo/1.0/',
+    contactUrl: 'https://about:notAtThemoMent',
   },
   reactStrictMode: true,
   swcMinify: true,
